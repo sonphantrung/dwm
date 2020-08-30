f46c948e81b3db543b0b8975b0efb767ea7bb198
sonphantrung <phantrungson17@gmail.com>
2020-08-30 18:08:24 +0700

    Minor Change

--- config.def.h
+++ config.def.h
@@ -10,7 +10,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Monospace:size=10" };
+static const char *fonts[]          = { "Mononoki Nerd Font:size=12" };
 static const char dmenufont[]       = "Mononoki Nerd Font:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
