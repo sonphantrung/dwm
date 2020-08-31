523608ca3a757ff846a9ea34bb655dac6454fa5a
sonphantrung <phantrungson17@gmail.com>
2020-08-31 16:41:21 +0700

    Undo

--- config.def.h
+++ config.def.h
@@ -12,11 +12,11 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Mononoki Nerd Font:size=12" };
 static const char dmenufont[]       = "Mononoki Nerd Font:size=12";
-static const char col_gray1[]       = "#B3222222";
-static const char col_gray2[]       = "#B3444444";
-static const char col_gray3[]       = "#b3bbbbbb";
-static const char col_gray4[]       = "#b3eeeeee";
-static const char col_cyan[]        = "#b3005577";
+static const char col_gray1[]       = "#222222";
+static const char col_gray2[]       = "#444444";
+static const char col_gray3[]       = "#bbbbbb";
+static const char col_gray4[]       = "#eeeeee";
+static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
