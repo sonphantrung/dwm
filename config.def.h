bf0ebf0b69cbca5b95f325f9cc301a7c298bfcae
sonphantrung <phantrungson17@gmail.com>
2020-09-25 18:30:56 +0700

    Rice

--- config.def.h
+++ config.def.h
@@ -25,7 +25,7 @@ static const char col_cyan[]        = "#88C0D0";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_cyan, col_gray1,  col_gray2  },
 };
 
 /* tagging */
