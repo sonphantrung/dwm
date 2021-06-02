7c0cc85f9f483a8d74e3f1edf460fe43c79f2772
Phan Trung Son <phantrungson17@gmail.com>
2021-06-02 18:25:05 +0700

    Add new keybindings

--- config.def.h
+++ config.def.h
@@ -104,6 +104,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,		XK_w,		spawn,		SHCMD("st -e nmtui; kill -45 $(pidof dwmblocks)") },
 	{ MODKEY|ShiftMask,		XK_p,		spawn,		SHCMD("st -e pulsemixer; kill -44 $(pidof dwmblocks)") },
 	{ MODKEY|ShiftMask,			XK_l,		spawn,		SHCMD("st -e lf") },
+	{ MODKEY|ShiftMask|ControlMask,		XK_w,		spawn,		SHCMD("wallsetter") },
 	{ MODKEY|ShiftMask,		XK_h,		spawn,		SHCMD("st -e htop") },
 	{ MODKEY|ShiftMask,		XK_c,		spawn,		SHCMD("clipmenu && herbe 'Copied'") },
 	{ Mod1Mask|ShiftMask,		XK_c,		spawn,		SHCMD("sysact") },
